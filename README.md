31d411e76a635798cde022b3ce1c24fa7a5a86a6
Damien Degois <damien@degois.info>
2022-10-11 19:57:05 +0200

    Switch query to callback, http_links param + gateway and subnet helpers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # phpipam-enhancer.user.js
 
-Simple helper [tampermonkey](https://www.tampermonkey.net/) script for daily use of phpipam.
+Simple helper [tampermonkey](https://www.tampermonkey.net/) script for daily use of [phpipam](https://www.tampermonkey.net/).
 
 The script adds 3 links next to IPs and hostnames:
  - http: opens ip or hostname as http link in a new tab
@@ -9,6 +9,6 @@ The script adds 3 links next to IPs and hostnames:
 
 ## Examples on demo.phpipam.net
 
-![Example on subnet view](https://github.com/babs/phpipam-enhancer.user.js/blob/master/img/demo.phpipam.net-subnetview.png)
+![Example on subnet view](https://github.com/babs/phpipam-enhancer.user.js/blob/main/img/demo.phpipam.net-subnetview.png)
 
-![Example on search result](https://github.com/babs/phpipam-enhancer.user.js/blob/master/img/demo.phpipam.net-search-result-dark.png)
+![Example on search result](https://github.com/babs/phpipam-enhancer.user.js/blob/main/img/demo.phpipam.net-search-result-dark.png)
--- phpipam-enhancer.user.js
+++ phpipam-enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         phpipam-enhancer
 // @namespace    https://github.com/babs/phpipam-enhancer.user.js
-// @version      0.7
+// @version      0.8
 // @downloadURL  https://github.com/babs/phpipam-enhancer.user.js/raw/main/phpipam-enhancer.user.js
 // @updateURL    https://github.com/babs/phpipam-enhancer.user.js/raw/main/phpipam-enhancer.user.js
 // @description  Enhance phpipam subnet view by adding http and https links to ips and hostnames
@@ -16,65 +16,97 @@
 
     var enhancements = [
         {
-            'query': '[class*=hostname]',
+            'getElements': () => document.querySelectorAll('td[class*=hostname]'),
             'valueidx': 0,
             'check_cb': (e) => {
                 return e.childNodes.length < 1 || e.childNodes[0].nodeType != XMLDocument.TEXT_NODE;
-            }
+            },
         },
         {
-            'query': '[class*=ipaddress]',
+            'getElements': () => document.querySelectorAll('td[class*=ipaddress]'),
             'valueidx': 1,
             'check_cb': (e) => {
                 return e.childNodes.length < 2 || e.childNodes[1].tagName != 'A';
-            }
+            },
         },
         {
-            'query': '[class=ip]',
+            'getElements': () => document.querySelectorAll('[class=ip]'),
             'valueidx': 0,
             'check_cb': (e) => {
                 return e.childNodes.length < 1 || e.childNodes[0].tagName != 'A';
-            }
+            },
         },
         {
-            'query': 'tr[class=ipSearch]>:nth-child(3)',
+            'getElements': () => document.querySelectorAll('tr[class=ipSearch]>:nth-child(3)'),
             'valueidx': 0,
             'check_cb': (e) => {
                 return e.childNodes.length < 1 || e.childNodes[0].nodeType != XMLDocument.TEXT_NODE;
-            }
+            },
+        },
+        {
+            'getElements': () => document.evaluate("//th[text()='Gateway']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.nextElementSibling.childNodes,
+            'valueidx': 0,
+            'check_cb': (e) => {
+                return e.childNodes.length < 1 || e.childNodes[0].nodeType != XMLDocument.TEXT_NODE;
+            },
+            'http_links': false,
+        },
+        {
+            'getElements': () => document.querySelectorAll('span[class=subnet_badge]'),
+            'valueidx': 0,
+            'check_cb': (e) => {
+                return false;
+            },
+            'http_links': false,
         },
     ]
 
-    enhancements.forEach( enh => {
-        for (let e of document.querySelectorAll(enh.query)) {
-            if (enh.check_cb(e)) {
-                continue;
+    let extend_element_with_helper = (element, value, http_links) => {
+        let value_for_links = value;
+        if (value.indexOf('::') !== false || value.split(':').length > 2) {
+            value_for_links = '['+value+']';
+        }
+
+        let httplink = document.createElement('a');
+        httplink.href = 'http://'+value;
+        httplink.text = 'http';
+        httplink.target = '_blank';
+
+        let httpslink = document.createElement('a');
+        httpslink.href = 'https://'+value;
+        httpslink.text = 'https';
+        httpslink.target = '_blank';
+
+        let copylink = document.createElement('a');
+        copylink.href = 'javascript:copyToClip("'+value+'");void(0)';
+        copylink.text = 'copy';
+
+        element.appendChild(document.createTextNode(' [\u00A0'));
+        if (http_links == undefined || http_links) {
+            element.appendChild(httplink);
+            element.appendChild(document.createTextNode('\u00A0|\u00A0'));
+            element.appendChild(httpslink);
+            element.appendChild(document.createTextNode('\u00A0|\u00A0'));
+        }
+        element.appendChild(copylink);
+        element.appendChild(document.createTextNode('\u00A0] '));
+
+    }
+
+    enhancements.forEach(enh => {
+        try {
+            for (let e of enh.getElements()) {
+                if (enh.check_cb(e)) {
+                    continue;
+                }
+                let value = e.childNodes[enh.valueidx].textContent.trim();
+                if (value == "") {
+                    continue;
+                }
+                extend_element_with_helper(e, value, enh.http_links);
             }
-            let value = e.childNodes[enh.valueidx].textContent.trim();
-            if (value == "") {
-                continue;
-            }
-            let httplink = document.createElement('a');
-            httplink.href = 'http://'+value;
-            httplink.text = 'http';
-            httplink.target = '_blank';
-
-            let httpslink = document.createElement('a');
-            httpslink.href = 'https://'+value;
-            httpslink.text = 'https';
-            httpslink.target = '_blank';
-
-            let copylink = document.createElement('a');
-            copylink.href = 'javascript:copyToClip("'+value+'");void(0)';
-            copylink.text = 'copy';
-
-            e.appendChild(document.createTextNode(' [\u00A0'));
-            e.appendChild(httplink);
-            e.appendChild(document.createTextNode('\u00A0|\u00A0'));
-            e.appendChild(httpslink);
-            e.appendChild(document.createTextNode('\u00A0|\u00A0'));
-            e.appendChild(copylink);
-            e.appendChild(document.createTextNode('\u00A0] '));
+        } catch(e) {
+            console.log('something whent wrong while processing ' + enh.getElements + ': ' + e);
         }
     });
     let script_elem = document.createElement('script');
